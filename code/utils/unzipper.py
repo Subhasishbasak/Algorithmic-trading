3891f26c560d0f6469f9b7412f5ded83aca46bae
subhasish <subhasish@cmi.ac.in>
2020-06-18 16:04:57 +0530

    unzipper script added

--- code/utils/unzipper.py
+++ code/utils/unzipper.py
@@ -1,10 +1,9 @@
 import os
-from tqdm import tqdm
 import zipfile
 
-path = ['../../resources/oneminutedata/2017', 
-        '../../resources/oneminutedata/2018']
-'''
+path = ['../../resources/2017', 
+        '../../resources/2018']
+
 for i in path:
     folders = os.listdir(i)
     for j in folders:
@@ -16,14 +15,7 @@ for i in path:
                 with zipfile.ZipFile(file_path, 'r') as zip_ref:
                     zip_ref.extractall(os.path.join(i,j))
                 print("sucessfully unzipped")   
-'''
-
-for i in path:
-    folders = os.listdir(i)
-    for j in folders:
-        files = os.listdir(os.path.join(i,j))
-        for k in files:
-            if k.endswith('.zip'):
-                file_path = os.path.join(i,j,k)
                 print("deleting file : ", file_path)
-                os.remove(file_path) 
+                os.remove(file_path)
+
+
