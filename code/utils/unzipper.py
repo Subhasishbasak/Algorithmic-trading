68a95b6c5714251bd2afa6d21db8ce9184db6cd0
subhasish <subhasish@cmi.ac.in>
2020-06-18 20:35:18 +0530

    unzipper modified

--- code/utils/unzipper.py
+++ code/utils/unzipper.py
@@ -1,20 +1,20 @@
 import os
 import zipfile
 
-path = ['../../resources/2017', 
-        '../../resources/2018']
+path = ['../../resources/2017', '../../resources/2018']
 
 for i in path:
     folders = os.listdir(i)
     for j in folders:
         files = os.listdir(os.path.join(i,j))
         for k in files:
-            if k.endswith('.zip'):
-                file_path = os.path.join(i,j,k)
+            file_path = os.path.join(i,j,k)
+            if k.endswith('.zip') and k.startswith('NIFTY'):
                 print("file unzipping : ", file_path)
                 with zipfile.ZipFile(file_path, 'r') as zip_ref:
                     zip_ref.extractall(os.path.join(i,j))
-                print("sucessfully unzipped")   
+                print("sucessfully unzipped")  
+            if k.endswith('.zip'):
                 print("deleting file : ", file_path)
                 os.remove(file_path)
 
